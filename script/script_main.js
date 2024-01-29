c0d89210318288422498a5f90a191c0f5f3a4fd6
Jakub <25148319+jakubekes@users.noreply.github.com>
2024-01-29 21:49:06 +0100

    proxmox_winhttp_file_download_and_changes

--- script/script_main.js
+++ script/script_main.js
@@ -14,109 +14,121 @@ var routes = {
 		contents_load_func("contact")
 	},
 	'/all': function () { 
-		contents_load_func("all")
+		contents_load_func("categories/all")
 	},
 	'/windows': function () { 
-		contents_load_func("windows")
+		contents_load_func("categories/windows")
     },
 	'/cpp': function () { 
-		contents_load_func("cpp")
+		contents_load_func("categories/cpp")
     },
 	'/linux': function () { 
-		contents_load_func("linux")
+		contents_load_func("categories/linux")
     },
 	'/virtualization': function () { 
-		contents_load_func("virtualization")
+		contents_load_func("categories/virtualization")
     },
 	'/7zip_batch_extract': function () { 
-		contents_load_func("7zip_batch_extract")
+		contents_load_func("articles/7zip_batch_extract")
     },  	  
 	'/7zip_batch_extract_de': function () { 
-		contents_load_func("7zip_batch_extract_de")
+		contents_load_func("articles/7zip_batch_extract_de")
     },
 	'/screenshot_taker': function () { 
-		contents_load_func("screenshot_taker")
+		contents_load_func("articles/screenshot_taker")
     },  	  
 	'/screenshot_taker_de': function () { 
-		contents_load_func("screenshot_taker_de")
+		contents_load_func("articles/screenshot_taker_de")
     },  	  
 	'/hmac_sha256': function () { 
-		contents_load_func("hmac_sha256")
+		contents_load_func("articles/hmac_sha256")
     },  	  
 	'/hmac_sha256_de': function () { 
-		contents_load_func("hmac_sha256_de")
+		contents_load_func("articles/hmac_sha256_de")
     },  	  
 	'/efi_partition_access_windows': function () { 
-		contents_load_func("efi_partition_access_windows")
+		contents_load_func("articles/efi_partition_access_windows")
     },  	  
 	'/efi_partition_access_windows_de': function () { 
-		contents_load_func("efi_partition_access_windows_de")
+		contents_load_func("articles/efi_partition_access_windows_de")
     },  	  
 	'/qemu_basics': function () { 
-		contents_load_func("qemu_basics")
+		contents_load_func("articles/qemu_basics")
     },  	  
 	'/qemu_basics_de': function () { 
-		contents_load_func("qemu_basics_de")
+		contents_load_func("articles/qemu_basics_de")
     },  	  
 	'/ps_batch_copy_from_zips': function () { 
-		contents_load_func("ps_batch_copy_from_zips")
+		contents_load_func("articles/ps_batch_copy_from_zips")
     },  	  
 	'/ps_batch_copy_from_zips_de': function () { 
-		contents_load_func("ps_batch_copy_from_zips_de")
+		contents_load_func("articles/ps_batch_copy_from_zips_de")
     },  	  
 	'/ffmpeg_video_tbnl_script': function () { 
-		contents_load_func("ffmpeg_video_tbnl_script")
+		contents_load_func("articles/ffmpeg_video_tbnl_script")
     },  	  
 	'/ffmpeg_video_tbnl_script_de': function () { 
-		contents_load_func("ffmpeg_video_tbnl_script_de")
+		contents_load_func("articles/ffmpeg_video_tbnl_script_de")
     },  	  
 	'/john-the-ripper_office_pwd_cracking': function () { 
-		contents_load_func("john-the-ripper_office_pwd_cracking")
+		contents_load_func("articles/john-the-ripper_office_pwd_cracking")
     },  	  
 	'/john-the-ripper_office_pwd_cracking_de': function () { 
-		contents_load_func("john-the-ripper_office_pwd_cracking_de")
+		contents_load_func("articles/john-the-ripper_office_pwd_cracking_de")
     },  	  
 	'/ps_hash_copy': function () { 
-		contents_load_func("ps_hash_copy")
+		contents_load_func("articles/ps_hash_copy")
     },  	  
 	'/ps_hash_copy_de': function () { 
-		contents_load_func("ps_hash_copy_de")
+		contents_load_func("articles/ps_hash_copy_de")
     },  	  
 	'/bootable_iso_and_physical_devices_with_VM': function () { 
-		contents_load_func("bootable_iso_and_physical_devices_with_VM")
+		contents_load_func("articles/bootable_iso_and_physical_devices_with_VM")
     },  	  
 	'/bootable_iso_and_physical_devices_with_VM_de': function () { 
-		contents_load_func("bootable_iso_and_physical_devices_with_VM_de")
+		contents_load_func("articles/bootable_iso_and_physical_devices_with_VM_de")
     },  	  
 	'/move_files_to_dirs_10k': function () { 
-		contents_load_func("move_files_to_dirs_10k")
+		contents_load_func("articles/move_files_to_dirs_10k")
     },  	  
 	'/move_files_to_dirs_10k_de': function () { 
-		contents_load_func("move_files_to_dirs_10k_de")
+		contents_load_func("articles/move_files_to_dirs_10k_de")
     },  	  
 	'/csv_divider': function () { 
-		contents_load_func("csv_divider")
+		contents_load_func("articles/csv_divider")
     },  	  
 	'/csv_divider_de': function () { 
-		contents_load_func("csv_divider_de")
+		contents_load_func("articles/csv_divider_de")
     },  	  
 	'/winhttp_api': function () { 
-		contents_load_func("winhttp_api")
+		contents_load_func("articles/winhttp_api")
     },  	  
 	'/winhttp_api_de': function () { 
-		contents_load_func("winhttp_api_de")
+		contents_load_func("articles/winhttp_api_de")
     },  	  
 	'/qt_multithreading_and_networking_basics': function () { 
-		contents_load_func("qt_multithreading_and_networking_basics")
+		contents_load_func("articles/qt_multithreading_and_networking_basics")
     },  	  
 	'/qt_multithreading_and_networking_basics_de': function () { 
-		contents_load_func("qt_multithreading_and_networking_basics_de")
+		contents_load_func("articles/qt_multithreading_and_networking_basics_de")
     },  	  
 	'/deluge_autoshutdown_python': function () { 
-		contents_load_func("deluge_autoshutdown_python")
+		contents_load_func("articles/deluge_autoshutdown_python")
     },  	  
 	'/deluge_autoshutdown_python_de': function () { 
-		contents_load_func("deluge_autoshutdown_python_de")
+		contents_load_func("articles/deluge_autoshutdown_python_de")
+    },  	  
+	'/winhttp_file_download': function () { 
+		contents_load_func("articles/winhttp_file_download")
+    },  	  
+	'/winhttp_file_download_de': function () { 
+		contents_load_func("articles/winhttp_file_download_de")
+    },  	  
+	'/proxmox_backup_handling_on_windows_host': function () { 
+		contents_load_func("articles/proxmox_backup_handling_on_windows_host")
+    },  	  
+	'/proxmox_backup_handling_on_windows_host_de': function () { 
+		contents_load_func("articles/proxmox_backup_handling_on_windows_host_de")
     }
 };
 
